6e4659779691150a7ee57fc334eb1182ebfb74d3
JyothiRadhaNanda <JyothiRadhaNanda@gmail.com>
2024-11-12 09:00:59 +0800

    (fixing)
    fixing map

--- pages/about.tsx
+++ pages/about.tsx
@@ -269,14 +269,15 @@ export default function About() {
         </div>
       </div>
       <div className=" flex flex-row gap-32 m-28">
-        <div>
-          <Image
-            src="/about/map.png"
-            alt="about Image"
-            width={1200}
-            height={200}
-            quality={100}
-            priority
+        <div className="w-1/2">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.846030212877!2d115.23796897589622!3d-8.706167791342807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd241bfead42cb1%3A0x24c6e2d6d2d87689!2sDimata%20IT%20and%20Software!5e0!3m2!1sid!2sid!4v1730960400785!5m2!1sid!2sid"
+            width="850"
+            height="650"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
           />
         </div>
         <div className="max-w-3xl place-content-center p-16">
